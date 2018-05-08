18872d362a5e9d665988c011a2e7efb73de2ad53
Luis Ovalle <luispalp@hotmail.com>
2018-05-08 03:59:56 -0600

    Se creo el proyecto y se hizo la primera maquina de turing

---
+++ Maquinas_de_turing/Proyecto2/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Proyecto2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbxInput = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btn_M5 = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.Cadena = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cabezal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(354, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbxInput
+            // 
+            this.tbxInput.Location = new System.Drawing.Point(114, 76);
+            this.tbxInput.Name = "tbxInput";
+            this.tbxInput.Size = new System.Drawing.Size(144, 20);
+            this.tbxInput.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(64, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(44, 41);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(114, 118);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(44, 41);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "2";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(164, 118);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(44, 41);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "3";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(214, 118);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(44, 41);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "4";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // btn_M5
+            // 
+            this.btn_M5.Location = new System.Drawing.Point(264, 118);
+            this.btn_M5.Name = "btn_M5";
+            this.btn_M5.Size = new System.Drawing.Size(44, 41);
+            this.btn_M5.TabIndex = 6;
+            this.btn_M5.Text = "5";
+            this.btn_M5.UseVisualStyleBackColor = true;
+            this.btn_M5.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgv.ColumnHeadersHeight = 20;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Cadena,
+            this.cabezal});
+            this.dgv.Location = new System.Drawing.Point(123, 197);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersWidth = 15;
+            this.dgv.Size = new System.Drawing.Size(135, 349);
+            this.dgv.TabIndex = 7;
+            // 
+            // Cadena
+            // 
+            this.Cadena.HeaderText = "Cadena";
+            this.Cadena.Name = "Cadena";
+            this.Cadena.ReadOnly = true;
+            this.Cadena.Width = 60;
+            // 
+            // cabezal
+            // 
+            this.cabezal.HeaderText = "Cabezal";
+            this.cabezal.Name = "cabezal";
+            this.cabezal.ReadOnly = true;
+            this.cabezal.Width = 60;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 574);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.btn_M5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tbxInput);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbxInput;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btn_M5;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cadena;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cabezal;
+    }
+}
+
---
+++ Maquinas_de_turing/Proyecto2/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto2
+{
+    public partial class Form1 : Form
+    {
+
+        string[] stn = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            LlenarDGV();
+
+            Maquina5(0, 1);
+
+
+        }
+
+        public void LlenarDGV()
+        {
+            dgv.Rows.Clear();
+            string input = tbxInput.Text;
+            string[] vinput = new string[input.Length];
+            dgv.Rows.Add(" ");
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                vinput[i] = input.Substring(i, 1);
+                dgv.Rows.Add(vinput[i]);
+            }
+
+            dgv.Rows.Add(" ");
+            stn = vinput;
+        }
+
+        public void Maquina5(int estado, int cabezal)
+        {
+
+            string head = null;
+
+            if (dgv.Rows[cabezal].Cells[0].Value == null)
+            {
+                head = " ";
+            }
+            else
+            {
+                head = dgv.Rows[cabezal].Cells[0].Value.ToString();
+            }
+
+            switch (estado)
+            {
+                case 0:
+
+                    dgv.Rows[cabezal].Cells[1].Value = "E0";
+
+                    if (head == "1")
+                    {
+                        estado = 0;
+                        dgv.Rows[cabezal].Cells[0].Value = "1";
+                        cabezal++;
+                    }
+                    else if(head == "-")
+                    {
+                        estado = 0;
+                        dgv.Rows[cabezal].Cells[0].Value = "-";
+                        cabezal++;
+                    }
+                    else if(head == null || head == " " || head == "")
+                    {
+                        estado = 1;
+                        dgv.Rows[cabezal].Cells[0].Value = " ";
+                        cabezal--;
+                    }
+
+                    Maquina5(estado, cabezal);
+
+                    break;
+                case 1:
+
+                    dgv.Rows[cabezal].Cells[1].Value = "E1";
+                    if (head == "1")
+                    {
+                        estado = 2;
+                        dgv.Rows[cabezal].Cells[0].Value = " ";
+                        cabezal--;
+                    }
+                    else if (head == "-")
+                    {
+                        estado = 4;
+                        dgv.Rows[cabezal].Cells[0].Value = " ";
+                        cabezal++;
+                    }
+                    else if (head == null || head == " ")
+                    {
+                         //no se puede
+                    }
+
+                    Maquina5(estado, cabezal);
+
+                    break;
+                case 2:
+
+                    dgv.Rows[cabezal].Cells[1].Value = "E2";
+                    if (head == "1")
+                    {
+                        estado = 2;
+                        dgv.Rows[cabezal].Cells[0].Value = "1";
+                        cabezal--;
+                    }
+                    else if (head == "-")
+                    {
+                        estado = 2;
+                        dgv.Rows[cabezal].Cells[0].Value = "-";
+                        cabezal--;
+                    }
+                    else if (head == null || head == " ")
+                    {
+                        estado = 3;
+                        dgv.Rows[cabezal].Cells[0].Value = " ";
+                        cabezal++;
+                    }
+
+                    Maquina5(estado, cabezal);
+
+                    break;
+                case 3:
+
+                    dgv.Rows[cabezal].Cells[1].Value = "E3";
+                    if (head == "1")
+                    {
+                        estado = 0;
+                        dgv.Rows[cabezal].Cells[0].Value = " ";
+                        cabezal++;
+                    }
+                    else if (head == "-")
+                    {
+                        estado = 4;
+                        dgv.Rows[cabezal].Cells[0].Value = "-";
+                        cabezal++;
+                    }
+                    else if (head == null || head == " ")
+                    {
+                        //no se puede
+                    }
+
+                    Maquina5(estado, cabezal);
+
+                    break;
+                case 4:
+
+                    dgv.Rows[cabezal].Cells[1].Value = "E4";
+                    break;
+            }
+        }
+    }
+}
